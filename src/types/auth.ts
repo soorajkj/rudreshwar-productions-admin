b922e2a1381c93c6cc3bcccad39719c87eb37422
soorajkj <soorajkj46@gmail.com>
2025-01-21 13:56:46 +0530

    fix: vercel build check

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,6 +1,3 @@
 import { SocialProviderList } from "better-auth/social-providers";
-import { auth } from "@/lib/auth";
 
 export type AppOAuthProvider = Extract<SocialProviderList, "google" | "github">;
-
-export type Session = typeof auth.$Infer.Session | null;
